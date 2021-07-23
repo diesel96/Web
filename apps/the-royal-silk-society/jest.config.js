eae74cb18d37bfc45c9fbcd4e75fbbb31e3849b0
diesel96 <jamesdanield@gmail,com>
2021-07-24 01:33:31 +0200

    initial commit

---
+++ apps/the-royal-silk-society/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'the-royal-silk-society',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/the-royal-silk-society',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
